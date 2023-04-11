6e405b852754cfd837c903a6bd1fbe42d1c6572d
Cole Brookson <cole.brookson@gmail.com>
2023-04-11 13:02:11 -0400

    doing the perm with just terrestrial daily first

--- R/manual/neon_perm_ent.R
+++ R/manual/neon_perm_ent.R
@@ -11,12 +11,24 @@
 # set up =======================================================================
 library(readr)
 library(here)
-
-
-
+library(tidyr)
 
 # going to go through the process once iwth one of the datasets, so for now I'll
 # just ues terrestrial-daily, for no particular reason
 terr_day <- readr::read_csv(
   here::here("./data/efi-neon-data/terrestrial-daily.csv")
   )
+
+# data set up ==================================================================
+
+names(terr_day)
+unique(terr_day$variable)
+
+# currently in long format, so going to pivot to a wide format
+terr_day_wide <- tidyr::pivot_wider(
+  data = terr_day, 
+  names_from = variable,
+  values_from = observation
+)
+
+
