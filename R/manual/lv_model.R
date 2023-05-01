cf66349678ce3925e5b88655a3f320613a2a4d8c
Cole Brookson <cole.brookson@gmail.com>
2023-05-01 16:25:35 -0400

    some predictability lv code

--- R/manual/lv_model.R
+++ R/manual/lv_model.R
@@ -16,7 +16,33 @@ times <- seq(0, 100, 0.1)
 
 
 # wrapper so we can get a sensitivity analysis going 
-lv_fme_wrapper <- function(parameters, times = seq(0, 100, 0.1)) {
+
+#' Lotka-Volterra equation solver with a wrapper
+#' 
+#' @description The lotka-volterra equation as `lv_model`, wrapped inside a 
+#' larger function to facilitate a sensitivity analysis. 
+#' 
+#' @param parameters named numeric vector. The parameters needed for the model 
+#' to run. Should be a named numeric vector of length 4 
+#' @param state named numeric vector. The initial state values of the system. 
+#' Should be named "x" and "y" respectively and passed as integers or numeric
+#' values. DEFAULT is c(x = 1, y = 2)
+#' @param times numeric vector.  DEFAULT is c(x = 1, y = 2)
+#' @returns A numeric vector.
+#' @examples
+#' lv_fme_wrapper()
+#' add(10, 1)
+lv_fme_wrapper <- function(parameters, state = c(x = 1, y = 2), 
+                           times = seq(0, 100, 0.1)) {
+  
+  # do type checks 
+  if(!is.numeric(parameters)) {
+    errorCondition("ERROR - parameters not a numeric vector")
+  }
+  if(any(names(parameters) != c("alpha", "beta", "delta", "gamma"))) {
+    errorCondition("ERROR - names of parameters incorrect")
+  }
+  
   # set initial conditions
   state <- c(x = 1, y = 2)
   # get the derrivative
@@ -28,5 +54,23 @@ lv_fme_wrapper <- function(parameters, times = seq(0, 100, 0.1)) {
     })
   }
   # do the solve within the wrapper
-  deSolve::lsoda(y = state, times = times, func = lv_model, parameters = pars)
+  deSolve::lsoda(y = state, times = times, func = lv_model, 
+                 parms = parameters)
 }
+
+# initial run
+lv_results <- lv_fme_wrapper(
+  parameters = pars
+)
+# https://strimas.com/post/lotka-volterra/
+lv_results %>% 
+  data.frame() %>% 
+  gather(var, pop, -time) %>% 
+  mutate(var = if_else(var == "x", "Prey", "Predator")) %>% 
+  ggplot(aes(x = time, y = pop)) +
+  geom_line(aes(color = var)) +
+  scale_color_brewer(NULL, palette = "Set1") +
+  labs(title = "Lotka-Volterra predator prey model",
+       subtitle = paste(names(pars), pars, sep = " = ", collapse = "; "),
+       x = "Time", y = "Population density") +
+  theme_base()
